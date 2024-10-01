131fc711b4077eb96dc1ef3ca814cf6c23292ce2
thanhduy-123 <DuyDBTSE173402@fpt.edu.vn>
2024-10-01 20:45:03 +0700

    add axios

--- koiPondManagement/src/components/config/axios.js
+++ koiPondManagement/src/components/config/axios.js
@@ -1,7 +1,19 @@
 import axios from "axios";
-// api swagger 
+// api swagger
 const api = axios.create({
-    baseURL: "htt[://14.225.220.131:8080/",
-  });
-  
+  baseURL: "htt[://14.225.220.131:8080/",
+});
+// làm 1 hành động gì đó trc khi call api
+const handleBefore = (config) => {
+  const token = localStorage.getItem("token")?.replaceAll('"', "");
+  config.headers["Authorization"] = `Bearer ${token}`;
+  return config;
+};
+
+const handleError = (error) => {
+  console.log(error);
+};
+
+api.interceptors.request.use(handleBefore, handleError);
+
 export default api;
