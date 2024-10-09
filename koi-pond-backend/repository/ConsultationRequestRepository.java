a5392889901836630614a3b9e6db43cf19f9f6a9
Your Name <you@example.com>
2024-10-09 10:28:55 +0700

    fix file dowload cannot connect da

--- koi-pond-backend/repository/ConsultationRequestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.koipond.backend.repository;
-
-import com.koipond.backend.model.ConsultationRequest;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ConsultationRequestRepository extends JpaRepository<ConsultationRequest, String> {
-}
